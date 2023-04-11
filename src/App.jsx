4b74096906ee4f212e3dc8a2f83ee1323789bc5d
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-04-11 13:45:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.scss";
+import Navbar from "./components/navbar.component/navbar.component";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
