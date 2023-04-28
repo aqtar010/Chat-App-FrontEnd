136f1a4e8634aff2ac6b10e6e21cbd2346ac934d
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-04-28 17:49:33 +0530

    communicat with front to back established

--- src/components/Home/home.component.jsx
+++ src/components/Home/home.component.jsx
@@ -1,10 +1,18 @@
-import './home.styles.scss'
+import { useEffect, useState } from "react";
+import "./home.styles.scss";
 
-const Home=()=>{
-    return (
-        <div className='home'>
-            <h1>This is home page</h1>
-        </div>
-    )
-}
+const Home = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  console.log(data);
+  return (
+    <div className="home">
+      <h1>This is home page</h1>
+    </div>
+  );
+};
 export default Home;
