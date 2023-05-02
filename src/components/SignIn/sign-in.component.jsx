0cd11b363ea612fea74cd8b61bc7ebc1cd29835c
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-02 22:57:47 +0530

    front to back data passing

--- src/components/SignIn/sign-in.component.jsx
+++ src/components/SignIn/sign-in.component.jsx
@@ -1,14 +1,57 @@
-import './sign-in.styles.scss'
+import { useState } from "react";
+import "./sign-in.styles.scss";
 
-const SignIn=()=>{
-    return <>
-    
-    <div className='sign-in-container'>
-        <input placeholder='Email' type='email'/>
-        <input placeholder='Password' type='password'/>
-        <button>Sign In</button>
-    </div>
+const SignIn = () => {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => {
+      return { ...values, [name]: value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:5000/submit", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputs)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <>
+      <div className="sign-in-container">
+        <form type="submit" onSubmit={handleSubmit}>
+          <input
+            name="Email"
+            placeholder="Email"
+            type="email"
+            onChange={handleChange}
+          />
+          <input
+            name="Password"
+            placeholder="Password"
+            type="password"
+            onChange={handleChange}
+          />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
     </>
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
