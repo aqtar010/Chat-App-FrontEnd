ff86a52f1551411f0f7b67d164898787829562c6
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-07 11:15:54 +0530

    sign up page ui set

--- src/components/Authentication/auth.component.jsx
+++ src/components/Authentication/auth.component.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
   };
 
   return (
-    <>
+    <div className="auth-container">
       <div className="sign-in-container">
         <h2>Sign In</h2>
         <form type="submit" onSubmit={handleSubmit} className="form">
@@ -40,31 +40,36 @@ const Auth = () => {
             name="Email"
             placeholder="Email"
             type="email"
+            required
             onChange={handleChange}
           />
           <input
             name="Password"
             placeholder="Password"
             type="password"
+            required
             onChange={handleChange}
           />
           <button type="submit">Sign In</button>
         </form>
+      </div>
+      <div className="sign-up-container">
         <form>
           <h2>Sign Up</h2>
-          <input name="Fname" placeholder="First Name" type="text" />
+          <input name="Fname" placeholder="First Name" type="text" required/>
           <input name="Lname" placeholder="Last Name" type="text" />
-          <input name="Email" placeholder="Email" type="email" />
-          <input name="Password" placeholder="Password" type="password" />
+          <input name="Email" placeholder="Email" type="email" required/>
+          <input name="Password" placeholder="Password" type="password" required/>
           <input
             name="ConfirmPass"
             placeholder="Confirm Password"
             type="password"
+            required
           />
           <button type="submit">Sign Up</button>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
