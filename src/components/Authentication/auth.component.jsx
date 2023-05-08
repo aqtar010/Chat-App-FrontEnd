5425e0bf03aa84264996222a3ec116118e1411a2
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-08 13:47:08 +0530

    sending sign up to backend

--- src/components/Authentication/auth.component.jsx
+++ src/components/Authentication/auth.component.jsx
@@ -2,17 +2,31 @@ import { useState } from "react";
 import "./auth.styles.scss";
 
 const Auth = () => {
-  const [inputs, setInputs] = useState({});
+  const [signInInputs, setsignInInputs] = useState({ email: "", password: "" });
+  const [signUpInputs, setsignUpInputs] = useState({
+    Fname: "",
+    Lname: "",
+    Email: "",
+    Password: "",
+    ConfirmPass: "",
+  });
 
-  const handleChange = (event) => {
+  const handleSignInChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs((values) => {
+    setsignInInputs((values) => {
+      return { ...values, [name]: value };
+    });
+  };
+  const handleSignupChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setsignUpInputs((values) => {
       return { ...values, [name]: value };
     });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmitSignIn = (event) => {
     event.preventDefault();
 
     fetch("http://localhost:5000/auth/sign-in", {
@@ -20,7 +34,7 @@ const Auth = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(inputs),
+      body: JSON.stringify(signInInputs),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -29,42 +43,100 @@ const Auth = () => {
       .catch((error) => {
         console.error("Error:", error);
       });
+    setsignInInputs({ email: "", password: "" });
+  };
+  const handleSubmitSignUp = (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:5000/auth/sign-up", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signUpInputs),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    setsignUpInputs({
+      Fname: "",
+      Lname: "",
+      Email: "",
+      Password: "",
+      ConfirmPass: "",
+    });
   };
 
   return (
     <div className="auth-container">
       <div className="sign-in-container">
         <h2>Sign In</h2>
-        <form type="submit" onSubmit={handleSubmit} className="form">
+        <form type="submit" onSubmit={handleSubmitSignIn} className="form">
           <input
             name="Email"
             placeholder="Email"
             type="email"
+            value={signInInputs.email}
             required
-            onChange={handleChange}
+            onChange={handleSignInChange}
           />
           <input
             name="Password"
             placeholder="Password"
             type="password"
+            value={signInInputs.password}
             required
-            onChange={handleChange}
+            onChange={handleSignInChange}
           />
           <button type="submit">Sign In</button>
         </form>
       </div>
+
       <div className="sign-up-container">
-        <form>
+        <form className="sign-up-form" onSubmit={handleSubmitSignUp}>
           <h2>Sign Up</h2>
-          <input name="Fname" placeholder="First Name" type="text" required/>
-          <input name="Lname" placeholder="Last Name" type="text" />
-          <input name="Email" placeholder="Email" type="email" required/>
-          <input name="Password" placeholder="Password" type="password" required/>
+          <input
+            name="Fname"
+            placeholder="First Name"
+            type="text"
+            value={signUpInputs.Fname}
+            required
+            onChange={handleSignupChange}
+          />
+          <input
+            name="Lname"
+            placeholder="Last Name"
+            value={signUpInputs.Lname}
+            type="text"
+            onChange={handleSignupChange}
+          />
+          <input
+            name="Email"
+            placeholder="Email"
+            type="email"
+            value={signUpInputs.Email}
+            required
+            onChange={handleSignupChange}
+          />
+          <input
+            name="Password"
+            placeholder="Password"
+            type="password"
+            value={signUpInputs.Password}
+            required
+            onChange={handleSignupChange}
+          />
           <input
             name="ConfirmPass"
             placeholder="Confirm Password"
             type="password"
+            value={signUpInputs.ConfirmPass}
             required
+            onChange={handleSignupChange}
           />
           <button type="submit">Sign Up</button>
         </form>
