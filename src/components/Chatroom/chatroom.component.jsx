2ba0406cbdbe5202b16b1ca6696552ac982d3c78
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-20 21:37:59 +0530

    chatroom update

--- src/components/Chatroom/chatroom.component.jsx
+++ src/components/Chatroom/chatroom.component.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import "./chatroom.styles.scss";
+import { useEffect } from "react";
 const ChatRoom = () => {
+  const [chatInput, setChatInput] = useState({});
+  const handleChatSend = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/chatroom/user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(chatInput),
+      });
+      if(!response.ok){
+        
+        throw new Error("Message sending failed");
+      }
+      const data = await response.json();
+        console.log("Message sent Success:", data);
+        
+        setChatInput({text:"",timestamp:""})
+
+    } catch (error) {
+      console.error("Error:", error.message);
+      // Handle error, show error message to the user, etc.
+    }
+  };
+  useEffect(() => {
+    console.log(chatInput);
+  }, [chatInput]);
+  const handleChatInputChange = (e) => {
+    setChatInput((values) => {
+      return { ...values, text: e.target.value, timestamp: Date.now() };
+    });
+  };
   return (
     <div className="chatroom-container">
       <div className="chatroom-header">
@@ -14,8 +47,14 @@ const ChatRoom = () => {
           </ul>
         </div>
         <div className="chat-area-conntainer">
-          <form type="submit">
-            <input type="textarea" placeholder="Enter Message"></input>
+          <form type="submit" onSubmit={handleChatSend}>
+            <div></div>
+            <input
+              type="textarea"
+              placeholder="Enter Message"
+              value={chatInput.text}
+              onChange={handleChatInputChange}
+            ></input>
             <button>Enter</button>
           </form>
         </div>
